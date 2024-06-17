8e543d84ef45336677f5a02f3a382332e972543a
Yuganshu <yuganshu219@gmail.com>
2024-06-17 20:08:12 +0530

    Minor adjustments

--- components/navbar/Searchbar.jsx
+++ components/navbar/Searchbar.jsx
@@ -15,12 +15,12 @@ export default function Searchbar({ updateReports }) {
 
     return (
         <form id='search-bar' onSubmit={handleSubmit} className="flex-1 flex items-center justify-center gap-2">
+            <input id='search-input' onChange={handleChange} value={location} className='w-[60%] h-[36px] font-medium flex-1 text-center rounded-sm text-black' type="text" placeholder='Search city...' />
             <button id='search-button' className='' type='submit'>
                 <svg id='search-icon' className='h-[36px]' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                 </svg>
             </button>
-            <input id='search-input' onChange={handleChange} value={location} className='w-[60%] h-[36px] font-medium flex-1 text-center rounded-sm text-black' type="text" placeholder='Search city...' />
         </form>
     )
 }
